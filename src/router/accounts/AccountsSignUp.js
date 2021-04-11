8ffb60f0c1871d1efb1d274a4107a4da1467832b
nagi0101 <demon0315@naver.com>
2021-04-11 20:43:35 +0900

    AccountsSignUp HTML/a bit of CSS

--- src/router/accounts/AccountsSignUp.js
+++ src/router/accounts/AccountsSignUp.js
@@ -1,7 +1,23 @@
 import "../../css/router/accounts/AccountsSignUp.css";
 
 function AccountsSignUp() {
-  return <div>AccountsSignUp</div>;
+  return (
+    <div className="AccountsSignUp">
+      <h1 className="logo">Masterpic's</h1>
+
+      <form>
+        <input placeholder="아이디" type="text" name="id" />
+        <input placeholder="비밀번호" type="password" name="password" />
+        <input
+          placeholder="비밀번호 확인"
+          type="password"
+          name="password_confirm"
+        />
+        <input placeholder="이메일" type="email" name="email" />
+        <button type="submit">가입하기</button>
+      </form>
+    </div>
+  );
 }
 
 export default AccountsSignUp;
