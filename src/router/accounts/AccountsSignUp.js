c84faaa5bfc137a6dbaec50cd3ee0ab1bf3ffad8
nagi0101 <demon0315@naver.com>
2021-04-12 02:41:03 +0900

    AccountSignUp CSS draftly done

--- src/router/accounts/AccountsSignUp.js
+++ src/router/accounts/AccountsSignUp.js
@@ -6,14 +6,14 @@ function AccountsSignUp() {
       <h1 className="logo">Masterpic's</h1>
 
       <form>
-        <input placeholder="아이디" type="text" name="id" />
-        <input placeholder="비밀번호" type="password" name="password" />
-        <input
-          placeholder="비밀번호 확인"
-          type="password"
-          name="password_confirm"
-        />
-        <input placeholder="이메일" type="email" name="email" />
+        <label htmlFor="id">아이디</label>
+        <input id="id" type="text" name="id" />
+        <label htmlFor="password">비밀번호</label>
+        <input id="password" type="password" name="password" />
+        <label htmlFor="password_confirm">비밀번호 확인</label>
+        <input id="password_confirm" type="password" name="password_confirm" />
+        <label htmlFor="email">이메일</label>
+        <input id="email" type="email" name="email" />
         <button type="submit">가입하기</button>
       </form>
     </div>
