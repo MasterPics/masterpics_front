c7990bce9a61b0ffc5211ca82127d3731103a9bd
nagiNotebook <demon0315@naver.com>
2021-04-13 15:11:35 +0900

    ProfileCard HTML done and move Header.scss to /src/scss

--- src/module/ProfileCard.js
+++ src/module/ProfileCard.js
@@ -1,7 +1,63 @@
 import "../css/module/ProfileCard.css";
 
-function ProfileCard() {
-  return <div className="ProfileCard">ProfileCard</div>;
+function ProfileCard({ user }) {
+  const {
+    username,
+    email,
+    desc,
+    category,
+    profile_img,
+    phone,
+    instagram,
+    portfolio_count,
+    contact_count,
+  } = user;
+  return (
+    <div className="ProfileCard">
+      <div>
+        <img
+          src={
+            profile_img ||
+            "https://cdn.iconscout.com/icon/free/png-512/account-profile-avatar-man-circle-round-user-30452.png"
+          }
+        />
+        <div>{username}</div>
+        <div>{category}</div>
+      </div>
+
+      <div>
+        <div>
+          <span>E-mail</span>
+          <span>{email}</span>
+        </div>
+        <div>
+          <span>Phone</span>
+          <span>{phone}</span>
+        </div>
+        <div>
+          <span>Instagram</span>
+          <span>{instagram}</span>
+        </div>
+      </div>
+
+      <button>메시지 보내기</button>
+
+      <div className="posts">
+        <div>
+          <span>포트폴리오</span>
+          <span>{portfolio_count}</span>
+        </div>
+        <div>
+          <span>컨택트</span>
+          <span>{contact_count}</span>
+        </div>
+      </div>
+
+      <div className="description">
+        <p>{desc}</p>
+      </div>
+    </div>
+  );
 }
 
 export default ProfileCard;
--- src/router/profile/ProfileLayout.js
+++ src/router/profile/ProfileLayout.js
@@ -10,16 +10,25 @@ function ProfileLayout({ match }) {
     username: "Nagi",
     email: "demon0315@naver.com",
     category: "other use",
+    phone: "010-2030-9131",
+    instagram: "bagjiin471",
+    portfolio_count: 10,
+    contact_count: 5,
     desc:
       "Nunc at erat viverra, consequat est ullamcorper, luctus mauris. Suspendisse commodo tortor ut lectus porta aliquam. Sed eu posuere purus. Mauris in tortor vehicula, consectetur neque quis, pharetra diam. Nullam luctus sapien risus, vitae posuere turpis venenatis a. Quisque ex magna, convallis ac congue sed, commodo sed purus. Integer euismod, ligula eget volutpat hendrerit, risus tortor eleifend tortor, in commodo augue magna cursus neque. Vestibulum congue massa lacus, sit amet aliquam metus facilisis aliquet.",
   };
 
   return (
     <div className="ProfileLayout">
-      <ProfileCard />
+      <ProfileCard user={user} />
 
       <Switch>
-        <Route path={`${match.path}/:id`} component={ProfileMain} />
+        <Route
+          path={`${match.path}/:id`}
+          render={() => {
+            <ProfileMain user={user} />;
+          }}
+        />
         <Route path={`${match.path}/:id/edit/`} component={ProfileEdit} />
       </Switch>
     </div>
