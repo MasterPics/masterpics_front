6fa35514b2e12b2645b2fd20f5adbb2f3bdb05e7
Jiin Park <demon0315@naver.com>
2021-04-28 18:58:05 +0900

    start using context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./css/reset.css";
 import "./css/App.css";
+import { ConstantsContext, Constants } from "./context/ConstantContext";
 import GNB from "./router/GNB";
 import Footer from "./router/Footer";
 import Main from "./router/Main";
@@ -12,21 +13,23 @@ import AccountsLayout from "./router/accounts/AccountsLayout";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Route path="/">
-          <GNB />
-        </Route>
-        <Switch>
-          <Route path="/" component={Main} exact />
-          <Route path="/contact" component={ContactMain} />
-          <Route path="/portfolio" component={PortfolioMain} />
-          <Route path="/profile/:id" component={ProfileLayout} />
-          <Route path="/accounts" component={AccountsLayout} />
-        </Switch>
-        <Route path="/">
-          <Footer />
-        </Route>
-      </BrowserRouter>
+      <ConstantsContext.Provider value={Constants}>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <Route path="/">
+            <GNB />
+          </Route>
+          <Switch>
+            <Route path="/" component={Main} exact />
+            <Route path="/contact" component={ContactMain} />
+            <Route path="/portfolio" component={PortfolioMain} />
+            <Route path="/profile/:id" component={ProfileLayout} />
+            <Route path="/accounts" component={AccountsLayout} />
+          </Switch>
+          <Route path="/">
+            <Footer />
+          </Route>
+        </BrowserRouter>
+      </ConstantsContext.Provider>
     </div>
   );
 }
